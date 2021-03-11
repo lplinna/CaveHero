a988cecfdec1c8d5f3fd610bf2bdb41d56189e6c
aclassyfox <samschmitz422@yahoo.com>
2021-03-10 18:18:34 -0600

    inventory for rocks and amethyst

---
+++ PlayerMovement/Assets/rock.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rock : MonoBehaviour
+{
+
+    public GameObject pickup;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    
+    }
+
+    private void OnDestroy()
+    {
+        Instantiate(pickup, transform.position, drop.transform.rotation);
+    }
+}
