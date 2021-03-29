2bd56841f98052be2c2ae494730e64da382892c6
toppop7 <65695818+toppop7@users.noreply.github.com>
2021-03-29 15:11:18 -0500

    Re-added MoneyCounter

---
+++ PlayerMovement/Assets/Player/MoneyCounter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.UI.Text;
+
+public class MoneyCounter : MonoBehaviour
+{
+   
+
+   
+    public void SetMoney(int muns)
+    {
+        gameObject.GetComponent<UnityEngine.UI.Text>().text = "MONEY: " + muns;
+    }
+
+
+
+}
