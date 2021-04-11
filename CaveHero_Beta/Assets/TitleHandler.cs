df824f5a4ee5de33ae21d850d6a24d01335cb22d
toppop7 <65695818+toppop7@users.noreply.github.com>
2021-04-11 17:33:35 -0500

    Added Title Screen
    Title screen seems to function normally

---
+++ CaveHero_Beta/Assets/TitleHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("TutorialLevel");
+
+    }
+
+
+    public void ExitGame()
+    {
+        Application.Quit();
+
+    }
+}
